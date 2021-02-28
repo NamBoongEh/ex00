ec3f1b1277147726900ba6d76f97879665440ece
NamBoongEh <67003390+NamBoongEh@users.noreply.github.com>
2021-02-28 23:48:27 +0900

    Add files via upload

--- nam2.sql
+++ nam2.sql
@@ -21,3 +21,51 @@ select *
 from tbl_board2;
 
 select * from tbl_board2 where bno>0;
+
+insert into tbl_board2 (bno, title, content, writer)
+(select seq_board.nextval, title, content, writer from tbl_board2);
+
+select *
+from tbl_board2
+order by bno+1 desc;
+
+select
+/*+ index_desc(tbl_board2 pk_board) */
+*
+from tbl_board2
+where bno>0;
+
+select
+/* full(tbl_board2) */
+*
+from tbl_board2
+order by bno desc;
+
+select rownum rn, bno, title
+from tbl_board2;
+
+select
+/*+ full(tbl_board2) */
+rownum rn, bno, title
+from tbl_board2 where bno>0
+order by bno;
+
+select
+/*+ index_asc(tbl_board2 pk_board) */
+rownum rn, bno, title, content
+from tbl_board2;
+
+select
+/*+ index_desc(tbl_board2 pk_board) */
+rownum rn, bno, title, content
+from tbl_board2
+where bno>0;
+
+select bno, title, content
+from(
+select
+/*+ index_desc(tbl_board2 pk_board) */
+rownum rn, bno, title, content
+from tbl_board2
+where rownum <=20)
+where rn>10;
