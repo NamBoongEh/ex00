9c155dc8853f63549025230466f0c4d62ca24c23
NamBoongEh <67003390+NamBoongEh@users.noreply.github.com>
2021-03-01 22:29:50 +0900

    Add files via upload

--- nam2.sql
+++ nam2.sql
@@ -69,3 +69,28 @@ rownum rn, bno, title, content
 from tbl_board2
 where rownum <=20)
 where rn>10;
+
+create table tbl_reply (
+    rno number(10,0),
+    bno number(10,0) not null,
+    reply varchar2(1000) not null,
+    replyer varchar2(50) not null,
+    replyDate date default sysdate,
+    updateDate date default sysdate
+    );
+    
+create sequence seq_reply;
+
+alter table tbl_reply add constraint pk_reply primary key (rno);
+
+alter table tbl_reply add constraint fk_reply_board
+foreign key (bno) references tbl_board (bno);
+
+select *
+from tbl_board
+where rownum<10
+order by bno desc;
+
+select *
+from tbl_reply
+order by rno desc;
